02ddbecba61b70d0505d4d57c9914b63dbb996aa
NhamPhanDinh <phandinhnham@gmail.com>
2014-03-26 09:36:19 +0700

    comit

--- SMMA/src/jp/ne/smma/EventList/AboutFragment.java
+++ SMMA/src/jp/ne/smma/EventList/AboutFragment.java
@@ -22,7 +22,6 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
--- SMMA/src/jp/ne/smma/EventList/MainActivity.java
+++ SMMA/src/jp/ne/smma/EventList/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
